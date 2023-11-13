90a317e0cd992ad2d721ef67aca622513acfeeb4
yanz <yanto@linknet.co.id>
2023-11-13 18:08:49 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'XDRMS',
+    'version': '1.8',
+    'category': 'DEBT/DRM',
+    'sequence': 15,
+    'summary': 'Track Lead Debt Recovery',
+    'website': 'https://xdrms.myai.my.id/',
+    'depends': [
+    ],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'assets': {
+
+        ],
+        'web.assets_backend': [
+        ],
+        'web.assets_tests': [
+
+        ],
+        'web.qunit_suite_tests': [
+
+        ],
+    },
+    'license': 'LGPL-3',
+}
